4d040c877523dd5dcdf0e20f319c3b3afafec11e
KevinF <kevinoflannagain@gmail.com>
2021-04-03 17:43:35 +0100

    updated

--- scripts/display-01.js
+++ scripts/display-01.js
@@ -1,42 +1,51 @@
-// Frank Poth 02/28/2018
+// Frank Poth 03/09/2018
 
-/* This Display class contains the screen resize event handler and also handles
-drawing colors to the buffer and then to the display. */
+/* This class hasn't changed much since part 1. All it does now is resize the canvas
+a bit differently and draw rectangles to the buffer. */
 
 const Display = function(canvas) {
 
-    this.buffer  = document.createElement("canvas").getContext("2d"),
-    this.context = canvas.getContext("2d");
-  
-    this.renderColor = function(color) {
-  
-      this.buffer.fillStyle = color;
-      this.buffer.fillRect(0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
-  
-    };
-  
-    this.render = function() { this.context.drawImage(this.buffer.canvas, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height); };
-  
-    this.resize = function(event) {
-  
-      var height, width;
-  
-      height = document.documentElement.clientHeight;
-      width  = document.documentElement.clientWidth;
-  
-      this.context.canvas.height = height - 32;
-      this.context.canvas.width = width - 32;
-  
-      this.render();
-  
-    };
-  
-    this.handleResize = (event) => { this.resize(event); };
-  
+  this.buffer  = document.createElement("canvas").getContext("2d"),
+  this.context = canvas.getContext("2d");
+
+  this.drawRectangle = function(x, y, width, height, color) {
+
+    this.buffer.fillStyle = color;
+    this.buffer.fillRect(Math.floor(x), Math.floor(y), width, height);
+
   };
-  
-  Display.prototype = {
-  
-    constructor : Display
-  
+
+  this.fill = function(color) {
+
+    this.buffer.fillStyle = color;
+    this.buffer.fillRect(0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
+
   };
+
+  this.render = function() { this.context.drawImage(this.buffer.canvas, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height); };
+
+  this.resize = function(width, height, height_width_ratio) {
+
+    if (height / width > height_width_ratio) {
+
+      this.context.canvas.height = width * height_width_ratio;
+      this.context.canvas.width = width;
+
+    } else {
+
+      this.context.canvas.height = height;
+      this.context.canvas.width = height / height_width_ratio;
+
+    }
+
+    this.context.imageSmoothingEnabled = false;
+
+  };
+
+};
+
+Display.prototype = {
+
+  constructor : Display
+
+};
